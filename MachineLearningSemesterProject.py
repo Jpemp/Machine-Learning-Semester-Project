c6a87ba354317d9f52593d02f1db47158518a32a
Jpemp <159839779+Jpemp@users.noreply.github.com>
2024-11-18 01:27:11 -0600

    Update MachineLearningSemesterProject.py

--- MachineLearningSemesterProject.py
+++ MachineLearningSemesterProject.py
@@ -4,9 +4,31 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Loading test and train dataset
-train_reviews = pd.read_csv('train.csv', delimiter=',')
-test_reviews = pd.read_csv('test.csv', delimiter=',')
+#Loading test and train dataset into pandas dataframes
+train_reviews = pd.read_csv('train.csv', delimiter=',', names=["Polarity", "Title", "Message"])
+test_reviews = pd.read_csv('test.csv', delimiter=',', names=["Polarity", "Title", "Message"])
+
+#If you wish to look at the pandas dataframes for the train and test datasets, un-comment the two statements below
+#print(train_reviews)
+#print(test_reviews)
+
+#Display Polarity Occurrences on Bar Graph
+train_bar = train_reviews.Polarity.value_counts()
+train_bar.plot.bar()
+plt.xlabel("Polarity")
+plt.ylabel("Number of Occurrences")
+plt.title("Number of times each polarity occurs in the train dataset")
+plt.show()
+plt.close()
+
+
+test_bar = test_reviews.Polarity.value_counts()
+test_bar.plot.bar()
+plt.xlabel("Polarity")
+plt.ylabel("Number of Occurrences")
+plt.title("Number of times each polarity occurs in the test dataset")
+plt.show()
+plt.close()
+
 
 #Train a model for sentiment analysis
-
