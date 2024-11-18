9bd64b4408e6562cce78d3c54c1036936cf6a551
Jpemp <159839779+Jpemp@users.noreply.github.com>
2024-11-17 18:14:39 -0600

    Update MachineLearningSemesterProject.py
    Loaded the train and test data into panda dataframes

--- MachineLearningSemesterProject.py
+++ MachineLearningSemesterProject.py
@@ -1,1 +1,12 @@
+#This project involves a classification of review data on a scale of 1 to 2, with 1 being negative and 2 being positive
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Loading test and train dataset
+train_reviews = pd.read_csv('train.csv', delimiter=',')
+test_reviews = pd.read_csv('test.csv', delimiter=',')
+
+#Train a model for sentiment analysis
 
